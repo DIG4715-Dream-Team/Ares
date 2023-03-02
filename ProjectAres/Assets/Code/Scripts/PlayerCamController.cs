b4964d6988a966952bdf18746f38a320571c3a6f
NuclearDrifting <nucleardrifting@gmail.com>
2023-03-02 10:25:17 -0500

    Script Update
    - Camera control modified and basically done
    
    - Rigidbody rotates with where camera is looking
    
    * Work on moving where you are looking

--- ProjectAres/Assets/Code/Scripts/PlayerCamController.cs
+++ ProjectAres/Assets/Code/Scripts/PlayerCamController.cs
@@ -4,20 +4,32 @@ public class PlayerCamController : MonoBehaviour
 {
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 5F;
-    public float sensitivityY = 5F;
-    public float minimumX = -90F;
-    public float maximumX = 90F;
-    public float minimumY = -90F;
-    public float maximumY = 90F;
-    float rotationY = 0F;
-    float rotationX = 0F;
+    public float sensitivityX;
+    public float sensitivityY;
+    public float minimumX;
+    public float maximumX;
+    public float minimumY;
+    public float maximumY;
+    float rotationY;
+    float rotationX;
+    [SerializeField]
+    private GameObject player;
+
+    void Start()
+    {
+        
+    }
 
     void Update()
     {
         MouseLook();
     }
 
+    void FixedUpdate()
+    {
+        //player.GetComponent<PlayerController>().rb.rotation = Quaternion.Euler(Rigidbody.rotation.eulerAngles + )
+
+    }
     private void MouseLook()
     {
         if (axes == RotationAxes.MouseXAndY)
@@ -30,17 +42,8 @@ public class PlayerCamController : MonoBehaviour
 
 
             transform.eulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            player.GetComponent<PlayerController>().rb.transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
 
-            transform.eulerAngles = new Vector3(-rotationY, transform.eulerAngles.y, 0);
         }
     }
 }
--- ProjectAres/Assets/Code/Scripts/PlayerController.cs
+++ ProjectAres/Assets/Code/Scripts/PlayerController.cs
@@ -7,57 +7,19 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float slidespeed;
     public float mouseSensitivity;
-    private Rigidbody rb;
+    public Rigidbody rb;
 
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 5F;
-    public float sensitivityY = 5F;
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-    public float minimumY = -360F;
-    public float maximumY = 360F;
-    float rotationY = 0F;
-    float rotationX = 0F;
     void Start()
     {
         speed = 4;
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxisRaw("Horizontal");
         float locomotion = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector3(hozMovement * speed, rb.velocity.y, locomotion * speed);
-
-        //MouseLook();
-    }
-
-    private void MouseLook()
-    {
-        if (axes == RotationAxes.MouseXAndY)
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
-
-
-            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-        }
-        else if (axes == RotationAxes.MouseX)
-        {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-        }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-
-            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-        }
     }
 }
