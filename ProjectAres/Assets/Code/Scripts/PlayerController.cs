5fa226606b3fa3bc82ffff52aeacec96b35e5d78
NuclearDrifting <nucleardrifting@gmail.com>
2023-03-04 17:12:28 -0500

    Coding should be done

--- ProjectAres/Assets/Code/Scripts/PlayerController.cs
+++ ProjectAres/Assets/Code/Scripts/PlayerController.cs
@@ -1,21 +1,31 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private float slideForce;
-    private float mouseSensitivity { get; }
     public Rigidbody rb;
 
     public int Health { get; private set; }
+    public bool Died { get; private set; }
+    public bool ReachedWater { get; private set; }
+    public bool GameOver { get; private set; }
 
-    public bool died { get; private set; }
-    public bool reachedWater { get; private set; }
-    public bool gameOver { get; private set; }
+    public string ActiveBuff { get; private set; }
+    public bool UsingAbility;
+    public bool InBush;
+
+    public string AbilityReady { get; private set; }
+    public string AbilityName;
+
+    private GameObject enemy;
+    private EnemyController Enemy;
+
     void Start()
     {
-        slideForce = 10;
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        Enemy = enemy.GetComponent<EnemyController>();
+        ActiveBuff = "";
         speed = 4;
         rb = GetComponent<Rigidbody>();
         Health = 20;
@@ -24,25 +34,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         PauseControl();
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            PushBuff();
+            AbilityReady = "Sneak";
         }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            AbilityReady = "Speed";
+        }
+        ActivateBuff();
     }
 
     void FixedUpdate()
     {
         PlayerMovement();
-        //TestingFunction();
-    }
-
-    public void TestingFunction()
-    {
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            reachedWater = true;
-            gameOver = true;
-        }
     }
 
     public void HealthManagement(int amount)
@@ -51,14 +56,64 @@ public class PlayerController : MonoBehaviour
 
         if (Health <= 0)
         {
-            died = true;
+            Died = true;
         }
     }
 
-    public void PushBuff()
+    public void BuffManagement(string name)
     {
-        rb.AddRelativeForce(Vector3.forward * slideForce, ForceMode.Impulse);
+        if (name == "Sneak")
+        {
+            UsingAbility = true;
+            ActiveBuff = "Sneak";
+            transform.gameObject.tag = "HiddenPlayer";
+            Enemy.trackingPlayer = false;
+        }
+        if (name == "Speed")
+        {
+            UsingAbility = true;
+            ActiveBuff = "Speed Boost";
+            speed = 6;
+        }
     }
+
+    public void ResetBuffs()
+    {
+        speed = 4;
+        transform.gameObject.tag = "Player";
+    }
+
+    public void BuffControl(string name)
+    {
+        if (AbilityName != null)
+        {
+            AbilityName = name;
+            AbilityReady = name;
+        }
+    }
+
+    public void ActivateBuff()
+    {
+        if (Time.timeScale == 1 && AbilityReady != null && Input.GetKeyDown(KeyCode.J))
+        {
+            BuffManagement(AbilityReady);
+        }
+    }
+
+    public void Hidding(bool inBush)
+    {
+        if (inBush == true)
+        {
+            transform.gameObject.tag = "HiddenPlayer";
+            Enemy.trackingPlayer = false;
+        }
+        if (inBush == false)
+        {
+            transform.gameObject.tag = "Player";
+        }
+
+    }
+
 
     private void PlayerMovement()
     {
